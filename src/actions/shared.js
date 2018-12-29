bbb0ad5184c57f1414e28e6041e714770fcb72e6
Milsonei dos Santos Cardoso <milsonei@gmail.com>
2018-12-29 10:20:43 -0200

    Componente compartilhado por toda a aplicação

---
+++ src/actions/shared.js
@@ -0,0 +1,23 @@
+import API from 'goals-todos-api'
+
+export const RECIEVE_DATA = 'RECIEVE_DATA';
+
+function receiveData(todos, goals){
+    return {
+        type: RECIEVE_DATA,
+        todos,
+        goals
+    }
+}
+
+export function handleInitialData(){
+    return (dispatch) => {                
+        Promise.all([
+                        API.fetchTodos(), 
+                        API.fetchGoals()
+                    ])
+               .then(([todos, goals]) => {
+                        dispatch(receiveData(todos, goals));
+                    });
+    }
+}
